86954a837c80dcdf6613d361103048974dd678bc
Oleh <oleh.leptso@gmail.com>
2022-11-16 13:48:52 +0200

    Style changes

--- src/components/Job/Job.js
+++ src/components/Job/Job.js
@@ -1,11 +1,21 @@
 import './Job.css'
 
 function Job({data}) {
-  return (
-    <div className='job'>
-        <p>{data.name}</p>
-    </div>
-  );
+    return (
+        <div className='job'>
+            <div className='picture'>
+                <img src={data.pictures[0]}></img>
+            </div>
+            <div className='info'>
+                <p className='title'>{data.title}</p>
+                <p className='gray'>Department name - Allgemeines Krankenhaus der Stadt Wien - AKH</p>
+                <p className='gray'>Vienna, Austria</p>
+            </div>
+            <div className='saved'>
+                
+            </div>
+        </div>
+    );
 }
 
 export default Job;
